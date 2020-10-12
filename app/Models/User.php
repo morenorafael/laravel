da6b2db6297fec901e5c4f14fbf7345f40be06d3
Rafael Moreno <alejandro8924@gmail.com>
2020-10-12 10:20:09 -0500

    mis cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * Class User
+ * @package App\Models
+ * @property-read int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $password
+ * @property-read \Carbon\Carbon $created_at
+ * @property-read \Carbon\Carbon $updated_at
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -17,9 +28,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
